78f76cfd3b50ecf947a45edc897c2d35601956e2
bbw-tes <sabina.teleskumar@lernende.bbw.ch>
2025-09-17 17:49:12 +0200

    PostService is done, now implementing it in to main | I'm tryting to create a treshure hunt

--- src/bbw/sabina/Main.java
+++ src/bbw/sabina/Main.java
@@ -1,12 +1,15 @@
 package bbw.sabina;
 import bbw.sabina.models.Post;
 import bbw.sabina.models.User;
+import bbw.sabina.services.PostService;
 
 public class Main {
     public static void main(String[] args) {
-        //Ein User Objet und ein Post Objekt erstellen
-        User user1 = new User("John", "123", false );
+        //postService Objekt
+        PostService postService = new PostService();
         Post post1 = new Post("OOP", "Ich kann es immer noch nicht", user1 );
+        Post post2 = new Post("Clouding", "Das ist auch etwas wo ich nicht kann", user1);
+        Post post3 = new Post("Java", "Why you, why you", user1);
 
         //User Object holen(lesen)
         System.out.println("Title: " + post1.getTitle());
---
+++ src/bbw/sabina/services/PostService.java
@@ -0,0 +1,28 @@
+package bbw.sabina.services;
+import bbw.sabina.models.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostService {
+    // Step 1: List to store all posts
+    private List<Post> posts = new ArrayList<>();
+
+    // Step 2: Methode to create a post
+    public Post createPost(String title, String content, User user) {
+        Post post = new Post(title, content, user);
+        // ADd new post to list
+        posts.add(post);
+        return post;
+    }
+
+    public List<Post> getAllPosts(){
+        return posts;
+    }
+
+    public void deletePost(int id) {
+        for (Post post : posts) {
+        }
+    }
+
+
+}
