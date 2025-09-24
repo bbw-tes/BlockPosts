129b8358e7d7b9551cda1cac5bd466215b00fb3a
bbw-tes <sabina.teleskumar@lernende.bbw.ch>
2025-09-24 16:45:48 +0200

    Method Createpost and GetAll is done | next step is to create a docker-compose and after that to create a delete method

--- src/bbw/sabina/Main.java
+++ src/bbw/sabina/Main.java
@@ -5,17 +5,19 @@ import bbw.sabina.services.PostService;
 
 public class Main {
     public static void main(String[] args) {
-        //postService Objekt
+        //1. Create postService Object
         PostService postService = new PostService();
-        Post post1 = new Post("OOP", "Ich kann es immer noch nicht", user1 );
-        Post post2 = new Post("Clouding", "Das ist auch etwas wo ich nicht kann", user1);
-        Post post3 = new Post("Java", "Why you, why you", user1);
 
-        //User Object holen(lesen)
+        // 2. create a user
+        User user1 = new User("John", "1234", true);
+
+        // 3. creating post via postService
+        Post post1 = postService.createPost("OOP", "ich kann es immer noch nicht", user1);
+
+        //fetch User Object
         System.out.println("Title: " + post1.getTitle());
         System.out.println("Content: " + post1.getContent());
         System.out.println("Author: " + post1.getUser().getUsername());
         System.out.println("Admin-Status: " + post1.getUser().isAdmin());
-
     }
 }
--- src/bbw/sabina/services/PostService.java
+++ src/bbw/sabina/services/PostService.java
@@ -7,14 +7,15 @@ public class PostService {
     // Step 1: List to store all posts
     private List<Post> posts = new ArrayList<>();
 
-    // Step 2: Methode to create a post
+    // Step 2: Create Methods
+    // Method: create a post
     public Post createPost(String title, String content, User user) {
         Post post = new Post(title, content, user);
         // ADd new post to list
         posts.add(post);
         return post;
     }
-
+    // Methode: fetch or load all List data
     public List<Post> getAllPosts(){
         return posts;
     }
